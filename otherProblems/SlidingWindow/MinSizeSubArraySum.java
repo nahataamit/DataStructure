ea5d78120c3457e43747b9a565a687e38688c634
anahata <anahata@paypal.com>
2020-04-12 21:10:07 -0700

    Binary Search and Sliding Window Problems

--- otherProblems/SlidingWindow/MinSizeSubArraySum.java
+++ otherProblems/SlidingWindow/MinSizeSubArraySum.java
@@ -1,4 +1,22 @@
 package otherProblems.SlidingWindow;
 
+/**
+ * Given an array of positive numbers and a positive number ‘S’,
+ * find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’.
+ * Return 0, if no such subarray exists.
+ */
 public class MinSizeSubArraySum {
+    public static int findMinSubArray(int S, int[] arr) {
+        // TODO: Write your code here
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int result = MinSizeSubArraySum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 3, 2 });
+        System.out.println("Smallest subarray length: " + result);
+        result = MinSizeSubArraySum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 8 });
+        System.out.println("Smallest subarray length: " + result);
+        result = MinSizeSubArraySum.findMinSubArray(8, new int[] { 3, 4, 1, 1, 6 });
+        System.out.println("Smallest subarray length: " + result);
+    }
 }
